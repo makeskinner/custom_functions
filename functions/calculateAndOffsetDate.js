178214aff105b282ecebc9dfbbd89c1d1b082511
Mark Skinner <147710503+makeskinner@users.noreply.github.com>
2025-06-09 17:50:29 +0100

    Revert "Delete calculateAndOffsetDate.js"
    This reverts commit e66c74899cda934a0e2f10716516cb710a13ec05.

---
+++ functions/calculateAndOffsetDate.js
@@ -0,0 +1,34 @@
+function calculateAndOffsetDate(oldDate, newDate, offsetDate, timeString) {
+  // Convert date strings to Date objects
+  const oldDateObj = new Date(oldDate);
+  const newDateObj = new Date(newDate);
+  const offsetDateObj = new Date(offsetDate);
+
+  // Calculate difference in days
+  const timeDiff = newDateObj.getTime() - oldDateObj.getTime();
+  const daysDiff = Math.round(timeDiff / (1000 * 3600 * 24));
+
+  // Offset the third date by the calculated difference
+  offsetDateObj.setDate(offsetDateObj.getDate() + daysDiff);
+
+  // If timeString is provided, set the time
+  if (timeString) {
+    const timeParts = timeString.split(':');
+    offsetDateObj.setHours(timeParts[0], timeParts[1], timeParts[2]);
+  } else {
+    // Set time to midnight if no timeString is provided
+    offsetDateObj.setHours(0, 0, 0, 0);
+  }
+
+  // Get the ISO string
+  const isoString = offsetDateObj.toISOString();
+
+  // Check if timeString is provided
+  if (timeString) {
+    return isoString; // Return full ISO string with time
+  } else {
+    // Extract the date part (YYYY-MM-DD)
+    const dateOnly = isoString.split('T')[0]; //comment
+    return dateOnly; // Return date only
+  }
+}
