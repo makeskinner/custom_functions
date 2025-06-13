bb4a0168c297237a27b52309fb0397135b93e4a5
Mark Skinner <147710503+makeskinner@users.noreply.github.com>
2025-06-13 12:15:50 +0100

    getUtf8ByteLength.js
    Calculates the byte length of a string using UTF-8 encoding.

---
+++ functions/getUtf8ByteLength.js
@@ -0,0 +1,26 @@
+// This function safely calculates the UTF-8 byte length of a string.
+function getUtf8ByteLength(str) {
+  if (typeof str !== 'string') {
+    return 0; // Return 0 if input is not a string
+  }
+  let byteLength = 0;
+  for (let i = 0; i < str.length; i++) {
+    const code = str.charCodeAt(i);
+    // Determine byte size based on character code
+    if (code < 0x80) {
+      byteLength += 1;
+    } else if (code < 0x800) {
+      byteLength += 2;
+    } else if (code < 0xd800 || code >= 0xe000) {
+      byteLength += 3;
+    } else {
+      // Handle surrogate pairs (4 bytes for characters like emojis)
+      i++;
+      byteLength += 4;
+    }
+  }
+  return byteLength;
+}
+
+// This line makes the function available to other files that 'require' it.
+module.exports = getUtf8ByteLength;
