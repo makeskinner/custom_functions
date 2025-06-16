733cf9621321e023c23abc05da76a49d8e056b71
Mark Skinner <147710503+makeskinner@users.noreply.github.com>
2025-06-16 10:51:02 +0100

    Update getUtf8ByteLength.js
    another description

--- functions/getUtf8ByteLength.js
+++ functions/getUtf8ByteLength.js
@@ -19,6 +19,6 @@ function getUtf8ByteLength(str) {
       byteLength += 4;
     }
   }
-  // Return num bytes. Make's native modules handle the rest of the process
+  // Return num bytes. Make's native modules
   return byteLength;
 }
