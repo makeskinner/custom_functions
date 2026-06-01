1ff248e1824839b08eab6e2e35e0516a052332c7
Mark Skinner <147710503+makeskinner@users.noreply.github.com>
2026-06-01 16:18:21 +0100

    Add ORG_ID to teams query for per-org indexing in JS

--- CodeApp/make_tva_sfdc_monday/teams_bixfunc_snowflake.sql
+++ CodeApp/make_tva_sfdc_monday/teams_bixfunc_snowflake.sql
@@ -1,4 +1,5 @@
 SELECT
+    f.ORG_ID,
     t.NAME AS team_name,
     sc.BUSINESS_FUNCTION,
     SUM(f.CREDITS) AS total_credits,
@@ -14,5 +15,5 @@ LEFT JOIN MAKE_DATA.DWH.DIM_SCENARIO_CATEGORIZATION sc
     ON f.ORG_SCN_ID = sc.ORG_SCN_ID
     AND sc.IS_LAST_VERSION = 1
 WHERE f.ORG_ID = '{{119.Make_LifeCycles__r.records[1].imt_Make_OrgId__c}}'
-GROUP BY t.NAME, sc.BUSINESS_FUNCTION
+GROUP BY f.ORG_ID, t.NAME, sc.BUSINESS_FUNCTION
 ORDER BY total_credits DESC;
