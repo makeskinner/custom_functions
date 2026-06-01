763ae836e44588f4241db4d0c13dafc40bc7bf23
Mark Skinner <147710503+makeskinner@users.noreply.github.com>
2026-06-01 11:51:18 +0100

    Snowflake added
    Added Snowflake queries

--- CodeApp/make_tva_sfdc_monday/oldSOQL.sql
+++ CodeApp/make_tva_sfdc_monday/oldSOQL.sql
@@ -11,102 +11,101 @@ SELECT
    imt_Company_Size__c,
    BillingCountry,
    BillingCountryCode,
-   ( 
-      SELECT
+   (
+     SELECT
          Id, 
          Name, 
          StageName, 
          AmountConvertedUSD__c, 
          CloseDate, 
+         CreatedDate, 
          Opportunity_Link__c, 
          Type, 
          imt_Churn_Risk__c, 
-         Close_FIscal_Year__c, 
-         FiscalQuarter, 
-         CreatedDate, 
-         imt_Churn_Status__c, 
-         imt_Churn_Approval_Status__c, 
-         imt_Churn_Request_Date__c, 
-         imt_Churn_Reason__c, 
-         imt_Churn_Request_Details__c, 
-         imt_Make_Estimated_Churn_Value__c, 
          Executive_Summary__c, 
          imt_Notes__c, 
          Next_Step__c, 
-         imt_Pre_Sales_confidence_for_Quarter__c, 
-         imt_Pre_Sales_Next_Steps__c, 
          imt_Tech_Risks_Gaps__c, 
-         RecordType.Name 
-      FROM
+         RecordType.Name,
+         imt_Churn_Status__c, 
+         imt_Churn_Reason__c, 
+         imt_Make_Estimated_Churn_Value__c
+     FROM
          Opportunities 
-      WHERE
-         IsClosed = true 
-         AND RecordType.Name IN ('O02','O04')
-      ORDER BY
-         CloseDate DESC 
-      LIMIT 1 
+     WHERE
+         Celonis_Business_Unit__c = 'Integromat/Make'
+         AND RecordType.DeveloperName IN ('O02', 'O04')
+         AND IsClosed = false
+         AND StageName NOT IN ('Rejected', 'Profile', 'Closed Lost', 'Closed Won')
+     ORDER BY
+         AmountConvertedUSD__c DESC 
    ),
-   ( 
+   (
       SELECT
-         Id, 
-         imt_Make_OrgId__c, 
-         imt_Org_Plan__c, 
-         imt_Usage_Score__c, 
-         imt_Org_Ops_In_Plan__c, 
-         imt_Org_Ops_Left_In_Plan__c, 
-         imt_Org_Ops_Left_In_Plan_w_Extra__c, 
-         imt_Org_Extra_Ops_In_Plan__c, 
-         imt_Exp_Consumption_End_Val_Period__c, 
-         List_of_Apps_Used__c, 
-         imt_FDESK_Company_URL__c, 
-         imt_Org_Active_Scenarios_Curr_Month__c, 
-         imt_Org_Active_Scenarios_Prev_Month__c, 
-         imt_Org_Nb_Teams_Current_Month__c, 
-         imt_Org_Nb_Teams_Previous_Month__c, 
-         imt_Org_Nb_Users_Curr_Month__c, 
-         imt_Org_Nb_Users_Prev_Month__c, 
-         imt_Org_Nb_Active_Users_Curr_Month__c, 
-         imt_Org_Ops_Consumed_Last_30d__c, 
-         imt_Org_Ops_Consumed_Prev_Month__c, 
-         imt_Org_Ops_Consumed_Curr_Month__c, 
-         Contract_Duration__c, 
-         Expansion_Potential__c, 
-         Sum_Renewal_Amount__c, 
-         CSAT_Average__c, 
-         CSAT_Count__c, 
-         HealthScore_Average__c, 
-         HealthScore_Count__c, 
-         NPS_Average__c, 
-         NPS_Count__c, 
-         Nb_of_Open_Tickets__c, 
-         Nb_of_Tickets__c, 
-         imt_Org_Zone_Name__c 
+         Id,
+         imt_Make_OrgId__c,
+         imt_Org_Plan__c,
+         imt_Usage_Score__c,
+         imt_Org_Ops_In_Plan__c,
+         imt_Org_Ops_Left_In_Plan__c,
+         imt_Org_Ops_Left_In_Plan_w_Extra__c,
+         imt_Org_Extra_Ops_In_Plan__c,
+         imt_Exp_Consumption_End_Val_Period__c,
+         List_of_Apps_Used__c,
+         imt_FDESK_Company_URL__c,
+         imt_Org_Active_Scenarios_Curr_Month__c,
+         imt_Org_Active_Scenarios_Prev_Month__c,
+         imt_Org_Nb_Teams_Current_Month__c,
+         imt_Org_Nb_Teams_Previous_Month__c,
+         imt_Org_Nb_Users_Curr_Month__c,
+         imt_Org_Nb_Users_Prev_Month__c,
+         imt_Org_Nb_Active_Users_Curr_Month__c,
+         imt_Org_Ops_Consumed_Last_30d__c,
+         imt_Org_Ops_Consumed_Prev_Month__c,
+         imt_Org_Ops_Consumed_Curr_Month__c,
+         Contract_Duration__c,
+         Expansion_Potential__c,
+         Sum_Renewal_Amount__c,
+         CSAT_Average__c,
+         CSAT_Count__c,
+         HealthScore_Average__c,
+         HealthScore_Count__c,
+         NPS_Average__c,
+         NPS_Count__c,
+         Nb_of_Open_Tickets__c,
+         Nb_of_Tickets__c,
+         imt_Org_Zone_Name__c
       FROM
          Make_LifeCycles__r 
       WHERE
          imt_Org_Plan__c = 'Enterprise' 
-      AND imt_Make_OrgId__c != null 
-      LIMIT 1 
+         AND imt_Make_OrgId__c != null 
+      ORDER BY
+         imt_Usage_Score__c DESC LIMIT 1 
    ),
-   ( 
+   (
       SELECT
-         Id, 
-         Subject, 
-         StartDateTime, 
-         Activity_Type__c, 
-         Activity_Date__c, 
-         Delivered__c, 
-         Owner.Name 
+        Id,
+        Subject,
+        StartDateTime,
+        EndDateTime,
+        Activity_Type__c,
+        Activity_Date__c,
+        Delivered__c,
+        Location,
+        TYPEOF Who
+            WHEN Contact THEN Name, Email
+            WHEN Lead THEN Name, Email
+        END
       FROM
          Events 
-      WHERE
-         Owner.Name = 'Mark Skinner' 
-      AND (NOT Owner.Name LIKE '%Paula Materin%')
-      AND Activity_Date__c >= {{formatDate(addMonths(now; - 12); "YYYY-MM-DD")}} 
-      AND Activity_Date__c <= {{formatDate(addDays(now; 60); "YYYY-MM-DD")}} 
-   ) 
-FROM 
+      WHERE Activity_Date__c >= {{formatDate(addMonths(now; -12); "YYYY-MM-DD")}}
+         AND Activity_Date__c <= {{formatDate(addDays(now; 60); "YYYY-MM-DD")}}
+   )
+FROM
    Account 
-WHERE 
-   Id = '{{83.Id}}'
-   AND imt_Make_Lead_VE__c = '{{103.markskinnerteammemberid}}'
+WHERE Id IN (SELECT AccountId FROM Opportunity
+  WHERE Celonis_Business_Unit__c = 'Integromat/Make'
+  AND RecordType.DeveloperName IN ('O02', 'O04')
+  AND IsClosed = false
+  AND StageName NOT IN ('Rejected', 'Profile'))
